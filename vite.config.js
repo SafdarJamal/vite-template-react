a376e71407a8dfd511322104c4feb96c14190e23
Safdar Jamal <safdar.dev@gmail.com>
2022-01-17 23:41:20 +0500

    Format vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-		jsxRuntime: 'classic',
-	})],
+  plugins: [
+    react({
+      jsxRuntime: 'classic',
+    }),
+  ],
 });
