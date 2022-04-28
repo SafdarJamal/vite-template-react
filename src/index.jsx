4e3ed4f94e9c0a99305c682e60370e2682da2c68
ppusher <thelastjediwarrior-cs50@yahoo.com>
2022-04-28 20:22:04 +0530

    updated react's createRoot render method

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </React.StrictMode>
+)
