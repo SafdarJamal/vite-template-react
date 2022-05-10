7b40bf43455846b63f27d2ffc5cac78bac1f61a7
Safdar Jamal <safdar.dev@gmail.com>
2022-05-11 00:35:35 +0500

    Update index.jsx
    - Fix: Import createRoot from "react-dom/client"

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
